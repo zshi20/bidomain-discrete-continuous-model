9f474936f92b8b1942ac5753d503830b5141dbbe
zshi20 <115652353+zshi20@users.noreply.github.com>
2026-02-27 09:23:49 -0600

    Update find_element_from_xy_coordinates.m

--- discrete domain/find_element_from_xy_coordinates.m
+++ discrete domain/find_element_from_xy_coordinates.m
@@ -1,11 +1,10 @@
 function which_elem = find_element_from_xy_coordinates(coords, node, elem)
 %FIND_ELEMENT_FROM_XY_COORDINATES this function finds the mesh element number that
-%corresponds to the provided x, y coordinates. 
-%
-% coords:     1x2 array of (x, y) coordinates (cant be more than one set of coordinate!)
-% node:       Nx2 node coordinate matrix
-% elem:       Mx3 connectivity matrix
-% which_elem: The index of the element the coordinate belongs into
+%corresponds to the provided x, y coordinates.
+%coords:     1x2 array of (x, y) coordinates (cant be more than one set of coordinate!)
+%node:       Nx2 node coordinate matrix
+%elem:       Mx3 connectivity matrix
+%which_elem: The index of the element the coordinate belongs into
 
 % A temporary variable (3x2 matrix) that contains the vertice coordinate
 % of the current triangle in the loop
@@ -22,9 +21,9 @@ function which_elem = find_element_from_xy_coordinates(coords, node, elem)
         J = [triangle_node(2,:)-triangle_node(3,:); 
              triangle_node(1,:)-triangle_node(3,:)]';
 
-        bary_node = J\(coords-triangle_node(3,:))';
+        bary_node = J\(coords-triangle_node(3,:))'; % Calculate the barycentric corrdinate
 
-        if bary_node(1) > 0 && bary_node(2) > 0 && bary_node(1) + bary_node(2) < 1  % if within the unit triangle
+        if bary_node(1) > 0 && bary_node(2) > 0 && bary_node(1) + bary_node(2) < 1  % if within the unit triangle (bary coord)
             which_elem = i;
             return;
         end      
@@ -33,6 +32,7 @@ function which_elem = find_element_from_xy_coordinates(coords, node, elem)
     % If no element is found, return -1
     which_elem = -1;
 end
+
 
 
 
